ef0755d4247754ea5a96b4acc64f15bf9f74bb66
Beesto <ramarajendran9585@gmail.com>
2025-12-13 09:25:10 +0530

    create model for campground

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require("express") ;
 const app = express() ;
 const path = require("path") ;
+const mongoose = require("mongoose") ;
+const CampgroundModel = require("./models/campground.js") ;
+
+mongoose.connect("mongodb://127.0.0.1:27017/CampReview")
+    .then(()=>{
+        console.log("db connected ") ;
+    })
+    .catch((err)=>{
+        console.log(err);
+        console.log("db not connected ") ;
+    })
+
+
+
 app.set("view engine","ejs") ;
 app.set("views",path.join(__dirname,"views")) ;
 
 app.get("/",(req,res)=>{
     res.render("home.ejs") ;
-})
+});
+
+app.get("/makecampground",async (req,res)=>{
+    const r1 = new CampgroundModel({title:"beach camping"});
+    await r1.save() ;
+    res.send(r1) ;
+});
+
 
 app.listen(3000,()=>{
     console.log("Server Initiated") ;
