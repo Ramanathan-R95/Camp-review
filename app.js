adc437e5db0399e662262a387a06ab00b1b044d0
Beesto <ramarajendran9585@gmail.com>
2025-12-18 03:00:49 +0530

    add creation of new campground

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/CampReview")
 
 app.set("view engine","ejs") ;
 app.set("views",path.join(__dirname,"views")) ;
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.render("home.ejs") ;
@@ -28,10 +29,22 @@ app.get("/campgrounds",async (req,res)=>{
     res.render("campgrounds/index.ejs",{campGrounds});
 });
 
+app.get("/campgrounds/new",(req,res)=>{
+    res.render("campgrounds/new.ejs");
+});
+
 app.get("/campgrounds/:id",async (req,res)=>{
     const {id} = req.params ;
     const camp = await CampgroundModel.findById(id);
     res.render("campgrounds/show.ejs",{camp});
+});
+
+
+app.post("/campgrounds",async(req,res)=>{
+    const campground = req.body.campground ;
+    const camp = new CampgroundModel(campground) ;
+    await camp.save();
+    res.redirect("/campgrounds") ;
 })
 
 
